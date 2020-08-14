d14f8d197dc2e49e7696d30f1846dba8cf87c0f8
callumfrance <france.callum@gmail.com>
2020-08-14 23:45:04 +0800

    inserting into html

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { DiceSet } from './DiceSet';
 
+const app = document.getElementById('app');
+const rollHTML = document.createElement('div');
+
 const q = new DiceSet().diceSet1();
 var result = q.rollSet();
 console.log(String(result[0] + ': ' + String(result[1])));
+
+rollHTML.innerHTML = (`<h1 id="total">` + String(result[0]) + `</h1>` +
+    `<h4 id="single_rolls">` + String(result[1]) + `</h4>`);
+
+app.appendChild(rollHTML);
